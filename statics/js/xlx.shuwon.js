69bfdfe23f1052cf97db85edaf8fb093938e9121
谌志义 <chenzy2@guahao.com>
2018-05-26 21:39:11 +0800

    100

--- statics/js/xlx.shuwon.js
+++ statics/js/xlx.shuwon.js
@@ -45,8 +45,6 @@ var xlx = {
 						move2();
 						clearInterval(timer)
 					}
-					alert("x:" + x);
-					alert("y:" + y);
 					var transform = 'translateX(' + x + 'px) translateY(' + y + 'px)'
 					$(".tracker").css({
 						transform: transform
@@ -75,8 +73,6 @@ var xlx = {
 						move3()
 						clearInterval(timer)
 					}
-					alert("move2 x:" + x);
-					alert("move2 y:" + y);
 					var transform = 'translateX(' + x + 'px) translateY(' + y + 'px)'
 					$(".tracker").css({
 						transform: transform
@@ -105,8 +101,6 @@ var xlx = {
 						move4()
 						clearInterval(timer)
 					}
-					alert("move3 x:" + x);
-					alert("move3 y:" + y);
 					var transform = 'translateX(' + x + 'px) translateY(' + y + 'px)'
 					$(".tracker").css({
 						transform: transform
@@ -135,8 +129,6 @@ var xlx = {
 						move1()
 						clearInterval(timer)
 					}
-					alert("move4 x:" + x);
-					alert("move4 y:" + y);
 					var transform = 'translateX(' + x + 'px) translateY(' + y + 'px)'
 					$(".tracker").css({
 						transform: transform
